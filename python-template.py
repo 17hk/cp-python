cfda02cf30bfc282cfe277a53e3ef87c7e11ed2e
dothk <harshitkhandelwal1705@gmail.com>
2022-10-07 13:36:07 +0530

    Add files via upload

---
+++ python-template.py
@@ -0,0 +1,37 @@
+def main():
+
+    def main():
+        pass 
+
+
+    def solve():
+        pass
+
+
+    main()
+
+
+
+MOD = 10**9 + 7
+
+
+
+#|__________________________________DANGER____________________________________|#
+
+import sys
+
+from math import ceil
+from collections import deque, Counter, defaultdict
+from bisect import bisect_left, bisect_right, insort
+from pprint import pprint
+
+
+read        = sys.stdin.readline
+readString  = lambda: read().strip()
+readInt     = lambda: int(readString())
+readStrings = lambda delim=' ': readString().split(delim)
+readInts    = lambda delim=' ': [int(_) for _ in readStrings(delim)]
+
+
+main()
+

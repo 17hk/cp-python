4c9ed4324d54c24cd08a42726cc1605b224e06e7
dothk <84710222+dothk@users.noreply.github.com>
2022-10-28 07:41:46 +0530

    edit

--- python-template.py
+++ python-template.py
@@ -1,42 +1,13 @@
-MOD = 10**9 + 7
-
-
-def main():
-
-    def main():
-        pass 
-
-
-    def solve():
-        pass
-
-
-    main()
-
-
-
-
-
-
-
 import sys
 
-from functools import *
-from itertools import *
-from math import *
-from collections import *
-from bisect import *
-from pprint import pprint
+def main():
+    pass
 
 
-read        = sys.stdin.readline
-readString  = lambda: read().strip()
-readInt     = lambda: int(readString())
-readStrings = lambda delim=' ': readString().split(delim)
-readInts    = lambda delim=' ': [int(_) for _ in readStrings(delim)]
+read, write, flush = sys.stdin.readline, sys.stdout.write, sys.stdout.flush
 
-
-sys.setrecursionlimit(10**6)
-main()
+if __name__ == '__main__':
+    sys.setrecursionlimit(10**6)
+    main()
 
 
